a28d546a25a9068e2b5ad9e6d542f93a046fdf64
mosesndubi275 <83883216+mosesndubi275@users.noreply.github.com>
2023-06-11 09:22:57 -0700

    Update and rename Jetbrains.py to banking.py

--- banking.py
+++ banking.py
@@ -1,13 +1,11 @@
+# Write your code here
 import random
 import sqlite3
-# Create new banking system
-"""Objective is to allow a cx to create a new account on our system by issuing them a new card number 
- and pin #. The card has to begin with 400000 followed by 10 digits.
- 
- I need a input system for the cx that allows them either create account, login, or exit the script"""
+import uuid
 
 options = '1. Create Account \n''2. Login \n' '0. Exit \n'
-second_options = '1. Balance \n' '2. Logout \n' '0. Exit \n'
+second_options = '1. Balance \n' '2. Add Income \n' '3. Do Transfer \n' '4. Close Account \n' '5. Log Out \n' '0. ' \
+                 'Exit \n'
 
 
 def menu():
@@ -29,7 +27,16 @@ def logged_in_menu():
         choice = 'Balance'
         return choice
     if number == 2:
-        choice = 'Logout'
+        choice = 'Add Income'
+        return choice
+    if number == 3:
+        choice = 'Do Transfer'
+        return choice
+    if number == 4:
+        choice = 'Close Account'
+        return choice
+    if number == 5:
+        choice = 'Log Out'
         return choice
     if number == 0:
         choice = 'Exit'
@@ -44,7 +51,7 @@ def create_card():
 
 
 def luhn_algorithm():
-    card_number = 4000004978139885
+    card_number = create_card()
     string_number = [int(i) for i in str(card_number)]
     string_number.pop(-1)
     range_ = range(0, 15)
@@ -76,17 +83,67 @@ def create_pin():
         pin = str(pin) + '0'
         return int(pin)
     else:
-        return random.randint(0000, 9999)
+        return pin
 
 
 login_screen = menu()
-user_card_information = {}
+connection = sqlite3.connect('card.s3db')
+cursor = connection.cursor()
+cursor.execute('Create TABLE IF NOT EXISTS card(id INTEGER PRIMARY KEY, number TEXT, pin TEXT, '
+               'balance INTEGER DEFAULT 0)')
+
+
+# Functions that directly deal with the database
+
+
+def data_base(id_number, card_number, pin_number, balance):
+    cursor.execute("Insert Into card VALUES (?,?,?,?)",
+                   (id_number, card_number, pin_number, balance))
+    connection.commit()
+    res2 = cursor.execute('Select * from card')
+    return res2.fetchall()
+
+
+def get_balance(pin_number):
+    res = cursor.execute(f"""SELECT balance 
+                            FROM card
+                            WHERE pin = {pin_number}""")
+    cards = res.fetchall()[0][0]
+    return cards
+
+
+def find_account(card_number, pin_number):
+    res = cursor.execute(f"""Select * FROM card WHERE number = {card_number} AND pin = {pin_number}""")
+    result = res.fetchall()[0]
+    return result
+
+
+def add_income(card_number, deposit_amount):
+    res = cursor.execute(f"""UPDATE card
+                            SET balance = {deposit_amount} + balance
+                            WHERE number = {card_number}""")
+    return
+
+
+def do_transfer(depositing_card, receiving_card):
+    try:
+        res = cursor.execute(f"Select * from card where number = {receiving_card}")
+        result = res.fetchall()
+        return result
+    except: raise(ValueError("CARD DOES NOT EXIST"))
+
+
+
+def close_account():
+    pass
+
 
 while login_screen != 'Exit':
     if login_screen == 'Create Account':
         customer_card = luhn_algorithm()
         customer_pin = create_pin()
-        user_card_information[customer_card] = customer_pin
+        unique_id = str(uuid.uuid4().int)[:4]
+        user_info = data_base(unique_id, customer_card, customer_pin, '0')
         message = (
             f"Your card has been created\n"
             f"Your card number:\n"
@@ -96,13 +153,13 @@ while login_screen != 'Exit':
         print(message)
     login_screen = menu()
     if login_screen == 'Login':
-        login_info = int(input('Enter your card info: '))
-        pin = int(input('Enter your pin: '))
-        if login_info in user_card_information and user_card_information[login_info] == pin:
+        login_info = input('Enter your card info: ')
+        pin = input('Enter your pin: ')
+        if login_info and pin in find_account(login_info, pin):
             print('You have Successfully Logged in! ')
             user_options = logged_in_menu()
             if user_options == 'Balance':
-                print(0)
+                print(f'Your balance is ${get_balance(str(pin))}')
                 user_options = logged_in_menu()
             if user_options == 'Logout':
                 print('You have Successfully Logged out! ')
@@ -112,5 +169,4 @@ while login_screen != 'Exit':
         else:
             print('Wrong user information')
     if login_screen == 'Exit':
-        user_card_information.clear()
         print('Bye!')
